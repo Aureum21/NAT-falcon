6faa6c250c2bf23dc00928a9a8647db2d5300e61
Aureum21 <alazarwake@gmail.com>
2024-08-07 09:27:48 +0300

    Modified August 7

--- home.py
+++ home.py
@@ -2,13 +2,12 @@ import streamlit as st
 import os
 import sqlite3
 import streamlit.components.v1 as components
-url = 'https://nat-falcon-main.streamlit.app/'
 
 st.set_page_config(layout="wide")
 
 script_dir = os.path.dirname(__file__)
 
-logo_path = "C:/Users/alazar/Desktop/falcon hackathon/alazar/falcon/logo.png" 
+logo_path = "logo.png" 
 
 def get_db_connection():
     try:
